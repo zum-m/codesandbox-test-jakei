ec72b3e10be02ac9aff89e43cab6aa01cc41b7b1
zum-m <89333894+zum-m@users.noreply.github.com>
2021-09-17 20:47:27 +0900

    todo app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-import "./styles.css";
+import "/src/styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストぼっくすの値を所得し初期化する🟡ここわからん
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // domの生成はクリエイトエレメントって関数使うみたい
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-raw";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // ---------------// bボタン作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    console.log("clickされた");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "消去";
+  deleteButton.addEventListener("click", () => {
+    // リストから消去
+  });
+
+  // --------------
+
+  // divタグの子要素に各区要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加🟡ここも三角
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+// ここの意味わからない。イベントについて復習する🟡
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
